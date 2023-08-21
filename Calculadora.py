b812d65316b8874688d1a510267436c40a692da3
valentina.insuasty <valentina01010206@gmail.com>
2023-08-20 19:09:03 -0500

    Se agrego operación de modulo

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
-print("Ingrese la operación (+, -, *, /, ^, **)")
+print("Ingrese la operación (+, -, *, /, **, %)")
 operacion = input()
 
 match operacion:
@@ -23,7 +23,12 @@ match operacion:
         # Operación división:
         division = numero1 / numero2
         print("La división es " + str(division))
-    case '^':
+    case '%':
+            # Operación modulo:
+            modulo = numero1 % numero2
+            print("El modulo es " + str(modulo))
+    case '**':
+    # Operación petencia:
         potencia = numero1 ** numero2
         print("La potencia es " + str(potencia))
     case _:
